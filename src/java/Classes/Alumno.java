665b4c7e697c098d24d5c4d54db9ea842e9088bf
molli <molli@Afrodita>
2020-09-21 09:41:46 -0500

    Updated

---
+++ src/java/Classes/Alumno.java
@@ -0,0 +1,86 @@
+package Classes;
+
+public class Alumno {
+    private int alumnos_id;
+    private String alumnos_nombre; 
+    private String alumnos_appaterno; 
+    private String alumnos_apmaterno; 
+    private String alumnos_numerocontrol;
+    private String alumnos_tipocurso; 
+    private int alumnos_semestre; 
+    private String alumnos_estatus;
+    private String alumnos_fcaptura;
+
+    public String getAlumnos_fcaptura() {
+        return alumnos_fcaptura;
+    }
+
+    public void setAlumnos_fcaptura(String alumnos_fcaptura) {
+        this.alumnos_fcaptura = alumnos_fcaptura;
+    }
+
+    public int getAlumnos_id() {
+        return alumnos_id;
+    }
+
+    public void setAlumnos_id(int alumnos_id) {
+        this.alumnos_id = alumnos_id;
+    }
+
+    public String getAlumnos_nombre() {
+        return alumnos_nombre;
+    }
+
+    public void setAlumnos_nombre(String alumnos_nombre) {
+        this.alumnos_nombre = alumnos_nombre;
+    }
+
+    public String getAlumnos_appaterno() {
+        return alumnos_appaterno;
+    }
+
+    public void setAlumnos_appaterno(String alumnos_appaterno) {
+        this.alumnos_appaterno = alumnos_appaterno;
+    }
+
+    public String getAlumnos_apmaterno() {
+        return alumnos_apmaterno;
+    }
+
+    public void setAlumnos_apmaterno(String alumnos_apmaterno) {
+        this.alumnos_apmaterno = alumnos_apmaterno;
+    }
+
+    public String getAlumnos_numerocontrol() {
+        return alumnos_numerocontrol;
+    }
+
+    public void setAlumnos_numerocontrol(String alumnos_numerocontrol) {
+        this.alumnos_numerocontrol = alumnos_numerocontrol;
+    }
+
+    public String getAlumnos_tipocurso() {
+        return alumnos_tipocurso;
+    }
+
+    public void setAlumnos_tipocurso(String alumnos_tipocurso) {
+        this.alumnos_tipocurso = alumnos_tipocurso;
+    }
+
+    public int getAlumnos_semestre() {
+        return alumnos_semestre;
+    }
+
+    public void setAlumnos_semestre(int alumnos_semestre) {
+        this.alumnos_semestre = alumnos_semestre;
+    }
+
+    public String getAlumnos_estatus() {
+        return alumnos_estatus;
+    }
+
+    public void setAlumnos_estatus(String alumnos_estatus) {
+        this.alumnos_estatus = alumnos_estatus;
+    }
+    
+}
